112b3f0bd8f6ece6d6e84f92ff96717e26f64188
Miller Peter <PMiller23>
2022-09-30 14:03:20 -0700

    Changed

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
     static Stack<Location> stack = new Stack<Location>(); //store moves in order (backtrack capability)
 
     //list of exhausted locations for each location.  Must use method convertLocToIndex to find a Locations proper index
-    static ArrayList<ArrayList<Location>> exhausted = new ArrayList<ArrayList<Location>>(rowL*colL);
+    static ArrayList<ArrayList<Location>> exhausted = new ArrayList<>(rowL * colL);
     static int board[][] = new int[rowL][colL];//2d array used to store the order of moves
     static boolean visited[][] = new boolean[rowL][colL];//2d array used to store what locations have been used
     static Location startLoc;
@@ -25,17 +25,39 @@ public class Main {
         initExhausted();
         ArrayList<Location> currentPossible;
         obtainStartLoc();
+        /*
         System.out.println("Start Loc is " + startLoc);
-        stack.push(startLoc);
         visited[startLoc.getRow()][startLoc.getCol()] = true;
         printBoard();
         addToExhausted(new Location(1,1), new Location(1,2));
         System.out.println("cleared 1, 1");
         clearExhausted(new Location(1,1));
         printExhausedList(new Location(1,1));
-        printPossibleMoveLocations(new Location(1,1));
+        printPossibleMoveLocations(new Location(1,2));
+        System.out.println(getNextMove(new Location(1,1),getPossibleMoves(new Location(1,1))));
+        printVisited();
+        printPossibleMoveLocations(new Location(4,4));
+
+         */
+
         while(stack.size() != rowL * colL && stack.size() != 0){
-            break;
+            Location top =stack.peek();
+            Location temp =getNextMove(top,getPossibleMoves(top));
+            if(temp!=null){
+                exhausted.get(convertLocToIndex(top)).add(temp);
+                stack.push(temp);
+                board[temp.getRow()][temp.getCol()]=stack.size();
+                printBoard();
+                printPossibleMoveLocations(top);
+            }else{
+                board[top.getRow()][top.getCol()]=0;
+                clearExhausted(stack.pop());
+            }
+        }
+        if(stack.size()==0){
+            System.out.println("No solution");
+        }else{
+            printBoard();
         }
 
 
@@ -48,6 +70,7 @@ public class Main {
         for(int i=0; i<exhausted.get(convertLocToIndex(loc)).size();i++){
             System.out.print(exhausted.get(convertLocToIndex(loc)).get(i));
         }
+        System.out.println();
     }
 
     /*
@@ -58,27 +81,29 @@ public class Main {
         for(int i=0; i<list.size();i++){
             System.out.print(list.get(i)+" ");
         }
+        System.out.println();
     }
 
     /*
      * prints out the board (numbers correspond to moves)
      */
-    public static void printBoard()
-    {
+    public static void printBoard() {
         for(int i=0; i<colL; i++){
             for(int j=0; j<rowL; j++){
                 System.out.print(board[i][j]+" ");
             }
             System.out.println();
         }
+        System.out.println();
     }
 
     /*
      * prints out true/false for what spaces have been visited
      */
-    public static void printVisited()
-    {
-
+    public static void printVisited(){
+        for (int i=0;i<stack.size();i++){
+            System.out.println(stack.get(i)+" ");
+        }
     }
 
     /*
@@ -96,7 +121,7 @@ public class Main {
      */
     public static void initExhausted() {
         for(int i=0;i<colL*rowL;i++)
-            exhausted.add(new ArrayList<Location>());
+            exhausted.add(new ArrayList<>());
     }
     /*
      * is this dest Location exhausted from the source Location
@@ -113,16 +138,35 @@ public class Main {
     /*
      * returns the next valid move for a given Location on a given ArrayList of possible moves
      */
-    public static Location getNextMove(Location loc, ArrayList<Location> list)
-    {
-        return null;
+    public static Location getNextMove(Location loc, ArrayList<Location> list) {
+        Location sending=null;
+        int inter=0;
+        for(int i=0; i<list.size();i++){
+            boolean send=true;
+            boolean blue;
+            if(!inExhausted(loc, list.get(i))){
+                for (int j=0; j<stack.size();j++) {
+                    Location listener=list.get(i);
+                    send = !(stack.get(j).getCol() == list.get(i).getCol() && (stack.get(j)).getRow() == list.get(i).getRow());
+                    blue = (board[(list.get(i).getRow())][(list.get(i).getCol())] == 0);
+                    if (send && blue) {
+                        inter = i;
+                        break;
+                    }
+                }
+                if(send){
+                    sending=list.get(inter);
+                    break;
+                }
+            }
+        }
+        return sending;
     }
 
     /*
      * converts a (row,col) to an array index for use in the exhausted list
      */
-    public static int convertLocToIndex(Location loc)
-    {
+    public static int convertLocToIndex(Location loc) {
         return (loc.getRow()*rowL) + loc.getCol();
     }
 
@@ -137,7 +181,7 @@ public class Main {
      * is this Location a valid one
      */
     public static boolean isValid(Location loc) {
-        return loc.getCol() <= colL && loc.getRow() <= rowL && loc.getRow() >= 0 && loc.getCol() >= 0;
+        return loc.getCol() < colL && loc.getRow() < rowL && loc.getRow() >= 0 && loc.getCol() >= 0;
     }
 
     /*
@@ -146,14 +190,14 @@ public class Main {
      */
     public static ArrayList<Location> getPossibleMoves(Location loc) {
         ArrayList<Location> list = new ArrayList<>();
-        list.add(new Location(loc.getCol()+1,loc.getRow()+2));
-        list.add(new Location(loc.getCol()-1,loc.getRow()+2));
-        list.add(new Location(loc.getCol()+1,loc.getRow()-2));
-        list.add(new Location(loc.getCol()-1,loc.getRow()-2));
-        list.add(new Location(loc.getCol()+2,loc.getRow()+1));
-        list.add(new Location(loc.getCol()+2,loc.getRow()-1));
-        list.add(new Location(loc.getCol()-2,loc.getRow()+1));
-        list.add(new Location(loc.getCol()-2,loc.getRow()-1));
+        list.add(new Location(loc.getRow()+1,loc.getCol()+2));
+        list.add(new Location(loc.getRow()-1,loc.getCol()+2));
+        list.add(new Location(loc.getRow()+1,loc.getCol()-2));
+        list.add(new Location(loc.getRow()-1,loc.getCol()-2));
+        list.add(new Location(loc.getRow()+2,loc.getCol()+1));
+        list.add(new Location(loc.getRow()+2,loc.getCol()-1));
+        list.add(new Location(loc.getRow()-2,loc.getCol()+1));
+        list.add(new Location(loc.getRow()-2,loc.getCol()-1));
         for(int i=7; i>=0;i--){
             if(!isValid(list.get(i))){
                 list.remove(i);
@@ -166,10 +210,10 @@ public class Main {
     /*
      * prompt for input and read in the start Location
      */
-    public static void obtainStartLoc()
-    {
+    public static void obtainStartLoc() {
         startLoc=new Location(0,0);
         board[startLoc.getCol()][startLoc.getRow()]=1;
+        stack.push(startLoc);
     }
 
 }
